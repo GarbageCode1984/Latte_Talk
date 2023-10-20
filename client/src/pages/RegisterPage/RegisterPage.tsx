a1d5aaa3f9e106829638e74e0c1eaafdaa9d4b46
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-10-20 19:35:40 +0900

    registerPage

--- client/src/pages/RegisterPage/RegisterPage.tsx
+++ client/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,24 +1,94 @@
 import { Link } from "react-router-dom";
 import "../../styles/RegisterPage.scss";
 import TextField from "@mui/material/TextField";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+    email: string;
+    password: string;
+    name: string;
+};
 
 const RegisterPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm<Inputs>({ mode: "onChange" });
+
+    const onSubmit: SubmitHandler<Inputs> = ({ email, password, name }) => {
+        console.log(email, password, name);
+        reset();
+    };
+
+    const userEmail: { required: string } = {
+        required: "필수 필드입니다.",
+    };
+
+    const userName: { required: string } = {
+        required: "필수 필드입니다.",
+    };
+
+    const userPassword: { required: string; minLength: { value: number; message: string } } = {
+        required: "필수 필드입니다.",
+        minLength: {
+            value: 8,
+            message: "최소 8자입니다.",
+        },
+    };
+
     return (
         <div className="layout">
             <div className="smartPhone">
                 <section>
                     <div>
-                        <h1 className="RegisterH1">회원가입</h1>
+                        <h1 className="registerH1">회원가입</h1>
 
-                        <form className="RegisterForm">
+                        <form className="registerForm" onSubmit={handleSubmit(onSubmit)}>
                             <div className="inputForm">
-                                <TextField id="email" label="Email" variant="outlined" type="email" />
+                                <TextField
+                                    id="email"
+                                    label="Email"
+                                    variant="outlined"
+                                    type="email"
+                                    {...register("email", userEmail)}
+                                />
+                                {errors?.email && (
+                                    <div>
+                                        <span className="errTeg">{errors.email.message}</span>
+                                    </div>
+                                )}
                             </div>
                             <div className="inputForm">
-                                <TextField id="name" label="Name" variant="outlined" type="text" />
+                                <TextField
+                                    id="name"
+                                    label="Name"
+                                    variant="outlined"
+                                    type="text"
+                                    {...register("name", userName)}
+                                />
+                                {errors?.name && (
+                                    <div>
+                                        <span className="errTeg">{errors.name.message}</span>
+                                    </div>
+                                )}
                             </div>
                             <div className="inputForm">
-                                <TextField id="password" label="Password" variant="outlined" type="password" />
+                                <TextField
+                                    id="password"
+                                    label="Password"
+                                    variant="outlined"
+                                    type="password"
+                                    {...register("password", userPassword)}
+                                />
+                                {errors?.password && (
+                                    <div>
+                                        <span className="errTeg">{errors.password.message}</span>
+                                    </div>
+                                )}
                             </div>
 
                             <div className="Reg">
@@ -27,7 +97,9 @@ const RegisterPage = () => {
                             </div>
 
                             <div className="registerButton">
-                                <button>회원가입</button>
+                                <Button variant="dark" size="lg">
+                                    회원가입
+                                </Button>
                             </div>
                         </form>
                     </div>
